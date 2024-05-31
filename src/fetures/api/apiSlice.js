a923d69f4d5eaadb1ab13a27e4f00da3f89e347d
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-05-31 22:35:57 +0600

    somde code added

--- src/fetures/api/apiSlice.js
+++ src/fetures/api/apiSlice.js
@@ -3,13 +3,14 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:7000/api/v1",
-    // baseUrl: "https://chatapplication-server-beta.vercel.app/api/v1",
+    baseUrl: "https://chatapplication-server-beta.vercel.app/api/v1",
+    credentials: "same-origin",
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
       const token = localStorage.getItem("accessToken");
       if (token) {
         headers.set("Authorization", `Bearer ${token}`);
+        headers.set("Content-Type", "application/json");
       }
       return headers;
     },
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./app/store.js";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router.jsx";
+import { store } from "./app/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
