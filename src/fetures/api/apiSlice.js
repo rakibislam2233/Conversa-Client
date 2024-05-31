a334e9f1b119c27a40fcfac5529c79024247cc60
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-05-31 21:45:07 +0600

    some code added

--- src/fetures/api/apiSlice.js
+++ src/fetures/api/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "http://localhost:7000/api/v1",
-    baseUrl: "https://chatapplication-server-beta.vercel.app/api/v1",
+    baseUrl: "http://localhost:7000/api/v1",
+    // baseUrl: "https://chatapplication-server-beta.vercel.app/api/v1",
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
       const token = localStorage.getItem("accessToken");
